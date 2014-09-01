acf0099a082e7e798189095355a6719b3a9e049a
Tomaka17 <pierre.krieger1708@gmail.com>
2014-09-01 22:42:24 +0200

    Minor example cleanup

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,25 +2,14 @@
 
 extern crate web_framework;
 
-pub struct CustomRequest<Rq> {
-    original: Rq
-}
-
-/*pub fn my_middleware<Rq: web_framework::Request, Rp: web_framework::Response>(request: Rq, response: Rp)
-    -> (CustomRequest<Rq>, Rp)
-{
-    (CustomRequest { original: request }, response)
-}*/
-
-
 fn main() {
     let server = web_framework::Server::new();
 
-    let server = server.with(web_framework::to_middleware(|&: rq, rp| {
-        (rq, rp)
+    let server = server.with(web_framework::to_middleware(|&: request, response| {
+        (request, response)
     }));
 
-    let server = server.with(web_framework::route::get(|&: _, _| {
+    let server = server.with(web_framework::route::get(|&: request, response| {
         use std::io::MemReader;
         //response.set_body(MemReader::new("hello world".as_bytes().to_vec()));
     }));
